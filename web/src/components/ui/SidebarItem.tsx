0d540ca7a017705c10b614aebd63f72ea0f3536b
shucream <shuya.uchida@gmail.com>
2024-02-11 13:37:44 +0900

    fix: build

--- web/src/components/ui/SidebarItem.tsx
+++ web/src/components/ui/SidebarItem.tsx
@@ -1,30 +1,33 @@
 import { useState } from "react"
 
+// @ts-ignore
 export default function SidebarItem({item}){
-    const [open, setOpen] = useState(false)
-
+  const [open, setOpen] = useState(false)
     
-    if(item.childrens){
-        return (
-            <div className={open ? "sidebar-item open" : "sidebar-item"}>
-                <div className="sidebar-title">
-                    <span>
-                        { item.icon && <i className={item.icon}></i> }
-                        {item.title}    
-                    </span> 
-                    <i className="bi-chevron-down toggle-btn" onClick={() => setOpen(!open)}></i>
-                </div>
-                <div className="sidebar-content">
-                    { item.childrens.map((child, index) => <SidebarItem key={index} item={child} />) }
-                </div>
-            </div>
-        )
-    }else{
-        return (
-            <a href={item.path || "#"} className="sidebar-item plain">
-                { item.icon && <i className={item.icon}></i> }
-                {item.title}
-            </a>
-        )
-    }
+  if(item.childrens){
+    return (
+      <div className={open ? "sidebar-item open" : "sidebar-item"}>
+        <div className="sidebar-title">
+          <span>
+            { item.icon && <i className={item.icon}></i> }
+            {item.title}
+          </span>
+          <i className="bi-chevron-down toggle-btn" onClick={() => setOpen(!open)}></i>
+        </div>
+        <div className="sidebar-content">
+          {
+            // @ts-ignore
+            item.childrens.map((child, index) => <SidebarItem key={index} item={child} />)
+          }
+        </div>
+      </div>
+    )
+  }else{
+    return (
+      <a href={item.path || "#"} className="sidebar-item plain">
+          { item.icon && <i className={item.icon}></i> }
+          {item.title}
+      </a>
+    )
+  }
 }
