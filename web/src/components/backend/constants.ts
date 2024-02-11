7f8011342d0ace788ba5513842529713b7e00c99
Yuto <yuto.y.0718@gmail.com>
2024-02-10 20:14:25 -0800

    Add .gitignore and package.json, and create new files for backend and login page

---
+++ web/src/components/backend/constants.ts
@@ -0,0 +1,8 @@
+import { Goerli, BaseGoerli } from "@thirdweb-dev/chains";
+
+export const THIRDWEB_API_KEY = process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID;
+
+export const chain = Goerli;
+
+export const factoryAddress = "0x3Bd745844685abab4B7034A108fa9994638011b4";
+
---
+++ web/src/components/backend/login.tsx
@@ -0,0 +1,103 @@
+import { useState } from "react";
+import styles from "./Home.module.css";
+import { connectToSmartWallet } from "./wallet";
+
+
+type LoginProps = {
+    isOpen: boolean;
+    onClose: () => void;
+};
+
+export const Login: React.FC<LoginProps> = ({ isOpen, onClose }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [signer , setSigner] = useState<any>(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [loadingStatus, setLoadingStatus] = useState('');
+    const [error, setError] = useState('');
+
+    if (!isOpen) return null;
+
+    const handleOutsideClick = (e: React.MouseEvent<HTMLDivElement>) => {
+        if (e.currentTarget === e.target) {
+            onClose();
+        }
+    };
+
+    const connectWallet = async () => {
+        if (!username || !password) return;
+        try {
+          setIsLoading(true);
+          const wallet = await connectToSmartWallet(username, password, (status) =>
+            setLoadingStatus(status)
+          );
+          const s = await wallet.getSigner();
+          setSigner(s);
+          setIsLoading(false);
+        } catch (e) {
+          setIsLoading(false);
+          console.error(e);
+          setError((e as any).message);
+        }
+      };
+
+    return username && signer ? (
+        <>
+           <p>Connected</p>
+        </>
+    ) : isLoading ? (
+        <div className={styles.card}>
+            <div
+                style={{
+                    width: "440px",
+                }}
+            >
+                {/* <LottieLoader animationData={loadingLottie}/> */}
+            </div>
+            <p>{loadingStatus}</p>
+        </div>
+    ) : error ? (
+        <div>
+            <p>Error</p>
+        <button onClick={() => setError('')}>
+            Try again
+        </button>
+        </div>
+    ) : (
+        <div
+            className={`${styles.loginContainer} ${isOpen ? styles.open : ''}`}
+            onClick={handleOutsideClick}
+        >
+                <div
+                    className={`${styles.loginCard} ${isOpen ? styles.open : ''}`}
+                >
+                    <button
+                    onClick={onClose}
+                    className={styles.closeButton}
+                    >x</button>
+                    <h1>Unilogin</h1>
+                    <div className={styles.loginInput}>
+                        <input
+                            type="text"
+                            placeholder="Username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <input 
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <button
+                            onClick={() => connectWallet()}
+                            >
+                                Login
+                            </button>
+                    </div>
+                </div>
+        </div>
+    );
+};
+
+
