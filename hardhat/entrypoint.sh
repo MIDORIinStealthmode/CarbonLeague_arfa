840e307105716854806d3224b7087a761b3474a2
森永祥平 <smorinaga@Mine-MBA.local>
2024-02-18 00:23:37 +0900

    update web/.env on local hardhat change

--- hardhat/entrypoint.sh
+++ hardhat/entrypoint.sh
@@ -8,9 +8,9 @@ npm run dev &
 # Wait for hardhat node to initialize and then deploy contracts
 npx wait-on http://127.0.0.1:8545 && yarn deploy:local;
 
-sed -i "s|CONTRACT_ADDR=.*|CONTRACT_ADDR=$(cat .contract)|g" .env && \
-sed -i "s|OWNER_ADDR=.*|OWNER_ADDR=$(cat .ownerAddress)|g" .env && \
-sed -i "s|ADMIN_ADDR=.*|ADMIN_ADDR=$(cat .adminAddress)|g" .env;
+sed -i "s|CONTRACT_ADDR=.*|CONTRACT_ADDR=$(cat .contract)|g" ./web/.env && \
+sed -i "s|OWNER_ADDR=.*|OWNER_ADDR=$(cat .ownerAddress)|g" ./web/.env && \
+sed -i "s|ADMIN_ADDR=.*|ADMIN_ADDR=$(cat .adminAddress)|g" ./web/.env;
 
 # The hardhat node process never completes
 # Waiting prevents the container from pausing
