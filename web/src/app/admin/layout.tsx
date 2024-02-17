04f67a85c52561effe4c9b669ead6b851ec26542
shucream <shuya.uchida@gmail.com>
2024-02-17 15:15:20 +0900

    chore(web): update package.json scripts to improve clarity and consistency feat(web): add new scripts for database operations in package.json for easier management style(web): refactor AdminNav component in AdminNav.tsx for better structure and readability refactor(web): remove unused CreateSuperpowerForm and page components in companies directory refactor(web): remove unused CreateSuperpowerForm and page components in superpowers directory style(web): update layout component in layout.tsx to include AdminNav component for better navigation in admin section

--- web/src/app/admin/layout.tsx
+++ web/src/app/admin/layout.tsx
@@ -1,15 +1,16 @@
 import {PropsWithChildren} from "react";
+import {AdminNav} from "@/app/admin/AdminNav";
 
 export default function AdminLayout({ children }: PropsWithChildren) {
   return (
     <div>
-      <header>
+      <header className="sticky top-0 inset-x-0 h-10 bg-black text-white">
         管理画面やで
       </header>
-      <nav>
-        
-      </nav>
-      <main>{children}</main>
+      <div className="flex">
+        <AdminNav />
+        <main>{children}</main>
+      </div>
     </div>
   )
 }
