4df358e361451a5b29fbbe1ce52d8180d13213c1
shucream <shuya.uchida@gmail.com>
2024-02-18 00:29:22 +0900

    feat(next.config.js): add async headers function to set CORS headers for /api/superpowers/:id route refactor(route.ts): remove redundant CORS header settings and simplify response creation by directly returning NextResponse object

--- web/src/app/api/superpowers/[nftId]/route.ts
+++ web/src/app/api/superpowers/[nftId]/route.ts
@@ -40,11 +40,5 @@ export const GET = async (request: Request, {params}: Params) => {
     ],
   }
 
-  const response = NextResponse.json(metadata)
-
-  response.headers.set("Access-Control-Allow-Origin", "*")
-  response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-  response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-
-  return response
+  return NextResponse.json(metadata)
 }
