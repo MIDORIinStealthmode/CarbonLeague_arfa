ecb4f39e12b211ca30e8c1f0120e9907f86ad9df
森永祥平 <smorinaga@Mine-MBA.local>
2024-02-18 04:10:56 +0900

    update web/.env

--- hardhat/entrypoint.sh
+++ hardhat/entrypoint.sh
@@ -8,7 +8,7 @@ npm run dev &
 # Wait for hardhat node to initialize and then deploy contracts
 npx wait-on http://127.0.0.1:8545 && yarn deploy:local;
 
-sed -i "s|SUPERPOWER_ADDRESS=.*|SUPERPOWER_ADDRESS=$(cat .contract)|g" ./web/.env && \
+sed -i "s|NEXT_PUBLIC_SUPERPOWER_ADDRESS=.*|NEXT_PUBLIC_SUPERPOWER_ADDRESS=$(cat .contract)|g" ./web/.env && \
 sed -i "s|OWNER_ADDRESS=.*|OWNER_ADDRESS=$(cat .ownerAddress)|g" ./web/.env && \
 sed -i "s|ADMIN_ADDRESS=.*|ADMIN_ADDRESS=$(cat .adminAddress)|g" ./web/.env;
 
