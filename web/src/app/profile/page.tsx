77b130d62369676480cb81ce8b1e4ec0f151a328
Yuto <yuto.y.0718@gmail.com>
2024-02-23 21:18:56 -0800

    Add navbar and sidebar components to profile page

--- web/src/app/profile/page.tsx
+++ web/src/app/profile/page.tsx
@@ -1,10 +1,18 @@
+'use client'
+import React from 'react';
 import { MyNFTs } from './myNFTList'
+import Sidebar from "@/components/ui/Sidebar";
+import Navbar from "@/components/ui/navbar";
 
 export default function ProfilePage() {
   return (
     <div>
+      <Navbar/>
+      <main className="">
+      <Sidebar />
       Profile
       <MyNFTs />
+      </main>
     </div>
   )
 }
