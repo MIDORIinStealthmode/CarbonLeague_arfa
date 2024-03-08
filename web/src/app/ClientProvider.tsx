66437fa8b2d44109879d60b3f17b74b6ca0ccd55
shucream <shuya.uchida@gmail.com>
2024-03-08 22:38:41 +0900

    fix

--- web/src/app/ClientProvider.tsx
+++ web/src/app/ClientProvider.tsx
@@ -4,12 +4,10 @@ import {PropsWithChildren} from "react";
 import {ThirdwebProvider, embeddedWallet, smartWallet} from "@thirdweb-dev/react";
 import {QueryProvider} from "@/app/QueryProvider";
 
-const activeChain = 'goerli'
-
 export const ClientProvider = ({ children }: PropsWithChildren) => {
   return (
     <ThirdwebProvider
-      activeChain={activeChain}
+      activeChain={process.env.NEXT_PUBLIC_CHAIN_ID!}
       clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}
       supportedWallets={[
         smartWallet(
