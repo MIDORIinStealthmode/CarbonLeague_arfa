001d89f18fe34acf48eef680da0edc7aa0c36a72
shucream <shuya.uchida@gmail.com>
2024-03-08 22:44:43 +0900

    fix

--- web/src/app/ClientProvider.tsx
+++ web/src/app/ClientProvider.tsx
@@ -3,11 +3,14 @@
 import {PropsWithChildren} from "react";
 import {ThirdwebProvider, embeddedWallet, smartWallet} from "@thirdweb-dev/react";
 import {QueryProvider} from "@/app/QueryProvider";
+import {Sepolia} from "@thirdweb-dev/chains";
+
+const activeChain = process.env.NEXT_PUBLIC_CHAIN_ID === '11155111' ? Sepolia : process.env.NEXT_PUBLIC_CHAIN_ID!
 
 export const ClientProvider = ({ children }: PropsWithChildren) => {
   return (
     <ThirdwebProvider
-      activeChain={process.env.NEXT_PUBLIC_CHAIN_ID!}
+      activeChain={activeChain}
       clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}
       supportedWallets={[
         smartWallet(
