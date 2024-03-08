cdec08f911e32afc1708f3ccd7c562f48136be6f
shucream <shuya.uchida@gmail.com>
2024-03-08 22:26:00 +0900

    fix(thirdweb.ts): update the hardcoded chain ID value to use the NEXT_PUBLIC_CHAIN_ID environment variable for flexibility and configuration.

--- web/src/lib/thirdweb.ts
+++ web/src/lib/thirdweb.ts
@@ -6,7 +6,7 @@ globalThis.TW_SKIP_FETCH_SETUP = true;
 
 export const sdk = ThirdwebSDK.fromPrivateKey(
   process.env.ADMIN_SECRET_KEY!,
-  'sepolia',
+  process.env.NEXT_PUBLIC_CHAIN_ID!,
   {
     secretKey: process.env.THIRDWEB_SECRET_KEY!,
   }
