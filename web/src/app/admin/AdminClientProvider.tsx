9c6f0836242a9edb5840dc1201df342521f40a7a
shucream <shuya.uchida@gmail.com>
2024-03-09 10:19:38 +0900

    fix admin query

---
+++ web/src/app/admin/AdminClientProvider.tsx
@@ -0,0 +1,12 @@
+import {PropsWithChildren} from "react";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+
+export const AdminClientProvider = ({ children }: PropsWithChildren) => {
+  const client = new QueryClient()
+
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+    </QueryClientProvider>
+  )
+}
