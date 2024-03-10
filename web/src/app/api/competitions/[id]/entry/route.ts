75312d30b129475cd1d1349635da745eaad704c5
shucream <shuya.uchida@gmail.com>
2024-03-10 12:48:08 +0900

    fix

--- web/src/app/api/competitions/[id]/entry/route.ts
+++ web/src/app/api/competitions/[id]/entry/route.ts
@@ -12,14 +12,17 @@ type Params = {
 }
 
 export const POST = async (request: Request, {params}: Params) => {
+  // 認証
   const user = await getUserModel()
 
   if (!user) {
     return new Response(null, { status: 401 })
   }
 
+  // パラメータ
   const rawData = await request.json()
   const result = CompetitionEntryBodySchema.safeParse(rawData)
+  const competitionId = params.id
 
   if (!result.success) {
     return new Response(null, { status: 400 })
@@ -28,7 +31,7 @@ export const POST = async (request: Request, {params}: Params) => {
   const superpowers = await prisma.superpower.findMany({
     where: {
       tokenId: {
-        in: result.data.tokenIds
+        in: result.data.tokenIds.map(i => Number(i))
       }
     }
   });
@@ -36,15 +39,28 @@ export const POST = async (request: Request, {params}: Params) => {
   if (superpowers.length !== 3) {
     return new Response(null, { status: 400 })
   }
-  
-  const entries = await prisma.competitionEntry.createMany({
-    data: superpowers.map((superpower, i) => ({
-      competitionId: params.id,
-      userId: user.id,
-      superpowerId: superpower.id,
-      order: i
-    }))
-  })
+
+  const query = superpowers.map((superpower, i) => (
+    prisma.competitionEntry.upsert({
+      where: {
+        competitionId_userId_order: {
+          userId: user.id,
+          competitionId: params.id,
+          order: i
+        }
+      },
+      update: {
+        superpowerId: superpower.id,
+      },
+      create: {
+        superpowerId: superpower.id,
+        userId: user.id,
+        competitionId: params.id,
+        order: i
+      },
+    })
+  ))
+  const entries = await prisma.$transaction([...query])
 
   return NextResponse.json(entries);
 }
