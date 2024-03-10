e63cecc14bd325b237e49d675185bd5c3ff2e0fe
shucream <shuya.uchida@gmail.com>
2024-03-10 17:58:05 +0900

    fix

--- web/src/app/(app)/competitions/page.tsx
+++ web/src/app/(app)/competitions/page.tsx
@@ -8,16 +8,10 @@ import {
   TableRow,
 } from "@/components/ui/table"
 import Link from "next/link";
-import prisma from "@/lib/prisma";
 import {EntrySheet} from "@/app/(app)/competitions/EntrySheet";
+import prisma from "@/lib/prisma";
 
 type CompetitionStatus = 'upcoming' | '開催中' | 'finished'
-
-type Competition = {
-  id: string
-  title: string
-  status: CompetitionStatus
-}
 
 export default async function CompetitionsPage() {
   const competitions = await prisma.competition.findMany({
