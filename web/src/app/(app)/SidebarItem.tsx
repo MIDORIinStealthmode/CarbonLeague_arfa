647123b72a2d07356f56fc8511d35b7ca128fc32
shucream <shuya.uchida@gmail.com>
2024-03-11 11:41:19 +0900

    fix

--- web/src/app/(app)/SidebarItem.tsx
+++ web/src/app/(app)/SidebarItem.tsx
@@ -12,7 +12,7 @@ export const SidebarItem = ({ href, children }: PropsWithChildren<Props>) => {
   const pathname = usePathname()
   const active = href.split('/')[1] === pathname.split('/')[1]
   return (
-    <Link href={href} className={`p-2 my-2 ${active && 'bg-neutral-500'}`}>
+    <Link href={href} className={`px-2 py-4 ${active && 'bg-neutral-500'} hover:bg-neutral-700`}>
       {children}
     </Link>
   )
