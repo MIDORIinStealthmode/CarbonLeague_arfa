cc0a195382bb42d9c876d775f0945e1431b21760
Yuto <yuto.y.0718@gmail.com>
2024-03-11 04:33:18 -0700

    APIの出力結果のsuperpowerData を res に含めるようにした。

--- web/src/app/competitions/results/ResultTable.tsx
+++ web/src/app/competitions/results/ResultTable.tsx
@@ -17,89 +17,107 @@ import {
 } from "@/components/ui/table"
 
 import {Category, Company, Superpower} from "@/lib/schema/zod";
-import {useMemo, useState} from "react";
+import {useEffect, useMemo, useState} from "react";
 import {Button} from "@/components/ui/button";
-import { ResultCall } from "./ResultCall";
 import Image from "next/image";
+import { set } from "zod";
+import { json } from "stream/consumers";
+
 
 type Props = {
-  superpowers: (Superpower & { company: Company, category: Category })[]
+  competitionId: string
+  newYear: number
 }
 
-export function ResultTable({superpowers}: Props) {
-  const columns: ColumnDef<Superpower>[] = useMemo(() => [
-    {accessorKey: "id"},
-    {accessorKey: "name"},
-    {
-      accessorKey: "imageUrl",
-      cell: ({ getValue }) => (
-        <Image
-          src={getValue() as string}
-          alt="NFTの画像"
-          className="w-20 h-20"
-          width={160}
-          height={160}
-        />
-      )
-    },
-    {accessorKey: "description"},
-    {accessorKey: "year"},
-    {accessorKey: "category.name"},
-    {accessorKey: "company.name"},
-    {accessorKey: "Score"},
-  ], [])
+export const ResultTable = ({ competitionId, newYear}: Props) => {
+  const [data, setData] = useState([])
+  const [address, setAddress] = useState('')
 
-  const table = useReactTable({
-    data: superpowers,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-  })
-  const rows = table.getRowModel().rows
+  useEffect(() => {
+  const fetchData = async () => {
+    const res = await fetch(`/api/admin/competitions/${competitionId}/database?newYear=${newYear}&competitionID=${competitionId}`);
+    const jsonData = await res.json()
+    setData(jsonData)
+    console.log(jsonData)
+  };
+  fetchData();
+  }, [competitionId, newYear]);
 
-  return (
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                  </TableHead>
-                )
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {rows?.length === 0 ? (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          ) : (
-            rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
+    const columns: ColumnDef<Superpower>[] = useMemo(() => [
+      {accessorKey: "id"},
+      {accessorKey: "tokenId"},
+      {accessorKey: "name"},
+      {
+        accessorKey: "imageUrl",
+        cell: ({ getValue }) => (
+          <Image
+            src={getValue() as string}
+            alt="NFTの画像"
+            className="w-20 h-20"
+            width={160}
+            height={160}
+          />
+        )
+      },
+      {accessorKey: "description"},
+      {accessorKey: "score"},
+      {accessorKey: "year"},
+      {accessorKey: "category.name"},
+      {accessorKey: "company.name"},
+    ], [])
+  
+    const table = useReactTable({
+      data: data,
+      columns,
+      getCoreRowModel: getCoreRowModel(),
+    })
+    const rows = table.getRowModel().rows
+  
+    return (
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                    </TableHead>
+                  )
+                })}
               </TableRow>
-            ))
-          )}
-        </TableBody>
-      </Table>
-    </div>
-  )
-}
+            ))}
+          </TableHeader>
+          <TableBody>
+            {rows?.length === 0 ? (
+              <TableRow>
+                <TableCell colSpan={columns.length} className="h-24 text-center">
+                  No results.
+                </TableCell>
+              </TableRow>
+            ) : (
+              rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    )
+  }
+  
--- web/src/app/competitions/results/page.tsx
+++ web/src/app/competitions/results/page.tsx
@@ -2,7 +2,7 @@
 
 import prisma from "@/lib/prisma";
 import {Button} from "@/components/ui/button";
-import { ResultCall } from "./ResultCall";
+import { ResultTable } from "./ResultTable";
 
 type Props = {
     competitionId: string;
@@ -13,8 +13,8 @@ type Props = {
 export default function ResultPage() {
   return (
     <div>
-      <h1>Result</h1>
-      <ResultCall 
+      <h1>Competition Result</h1>
+      <ResultTable
         competitionId="60eef0b9-d9f0-4d29-b7c2-f603107a9c7f"
         newYear={2022}
       />
