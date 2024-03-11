3ac200ac52c1f9ce676c22173da1e191f2bef64f
shucream <shuya.uchida@gmail.com>
2024-03-11 11:41:19 +0900

    fix

--- web/src/app/(app)/WalletButton.tsx
+++ web/src/app/(app)/WalletButton.tsx
@@ -1,12 +1,20 @@
 'use client'
 
 import {ConnectWallet} from "@thirdweb-dev/react";
+import {useRouter} from "next/navigation";
 
-export const WalletButton = () => (
-  <ConnectWallet
-    dropdownPosition={{
-      side: "bottom",
-      align: "center",
-    }}
-  />
-)
+export const WalletButton = () => {
+  const router = useRouter()
+
+  return (
+    <ConnectWallet
+      dropdownPosition={{
+        side: "bottom",
+        align: "center",
+      }}
+      auth={{
+        onLogin: router.refresh
+      }}
+    />
+  )
+}
